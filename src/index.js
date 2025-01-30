5a4b426ab3b0f39a0f9b9a97690ad504e4de2dbe
Sasha <st087955@student.spbu.ru>
2025-01-30 22:04:52 +0300

    Создание формы

--- src/index.js
+++ src/index.js
@@ -52,6 +52,11 @@ function createCard(task) {
         const classes = check.classList;
         classes.toggle('completeTask')
     })
+
+    btn.addEventListener('click', (e) => {
+        console.log(e.target.parentElement.classList);
+        drawForm(taskList.toDos[0]);
+    })
     return card
 }
 
@@ -72,6 +77,62 @@ function drawCards(lst){
 function compareTasks(task, cardNodes) {
     let card = cardNodes.childNodes;
     return (task.title == card[1].textContent )
+}
+
+function drawForm(info) {
+    const form = document.createElement('form'),
+        title = document.createElement('input'),
+        titleDiv = document.createElement('div'),
+        doneDiv = document.createElement('div'),
+        dateDiv = document.createElement('div'),
+        priorityDiv = document.createElement('div'),
+        lableTitle = document.createElement('label'),
+        done = document.createElement('input'),
+        labelDone = document.createElement('label'),
+        date = document.createElement('input'),
+        labelDate = document.createElement('label'),
+        priority = document.createElement('select'),
+        labelPriority = document.createElement('label'),
+        content = document.querySelector('#content');
+
+    title.type = 'text';
+    title.id = 'title';
+    title.value = info.title;
+    lableTitle.setAttribute('for', 'title');
+    lableTitle.textContent = 'title: ';
+    titleDiv.append(lableTitle, title);
+
+    done.type = 'checkbox';
+    done.id = 'done';
+    if (info.done == true) done.checked = true;
+    labelDone.setAttribute('for', 'done');
+    labelDone.textContent = 'done: ';
+    doneDiv.append(labelDone, done);
+    
+    date.type = 'date';
+    date.id = 'dueDate';
+    date.value = format(info.dueDate, 'yyyy-MM-dd');
+    labelDate.setAttribute('for', 'dueDate');
+    labelDate.textContent = 'deadline: '
+    dateDiv.append(labelDate, date);
+    
+    priority.id = 'priority';
+    for (let el of ['low', 'middle', 'high']){
+        addOption(priority, el, el);
+    }
+    priority.value = info.priority;
+    labelPriority.setAttribute('for', 'priority');
+    labelPriority.textContent = 'priority: ';
+    priorityDiv.append(labelPriority, priority);
+
+    form.append(titleDiv, doneDiv, dateDiv, priorityDiv);
+    form.className = 'form';
+    content.append(form);
+}
+
+function addOption(select, text, value){
+    const newOption = new Option(text, value);
+    select.options[select.options.length] = newOption;
 }
 
 taskList.addTask(new Task('afki', new Date(2025, 1, 1), 'low', 'home', true));
